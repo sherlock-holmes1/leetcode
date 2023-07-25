95e8476d171d99258f69de88a29d6921cb7bb5d5
Vasili Lukinykh <basil@powerinside.com>
2023-07-25 17:01:01 -0400

    17.Letters combinations for a phone number

--- 17.LetterCombinationsofaPhoneNumber.py
+++ 17.LetterCombinationsofaPhoneNumber.py
@@ -3,7 +3,9 @@ from typing import List
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        res = []
+        if digits == '':
+            return []
+        
         map = {2:'abc',
                3:'def',
                4:'ghi',
@@ -12,12 +14,20 @@ class Solution:
                7:'pqrs',
                8:'tuv',
                9:'wxyz'}
-        
-        ln = len(digits)
-        comb = ''
 
-        for i in range(ln - 1): # digits
-            ltrs = map[int(digits[i])]
-            
-            for j in range(len(ltrs) - 1): # letters for a digit
-                comb += ltrs[j]
+        if len(digits) == 1:
+            return [c for c in map[int(digits[0])]]
+        
+        else: 
+            cur_ltrs = [c for c in map[int(digits[0])]]
+            letter_combs = self.letterCombinations(digits[1:])
+            res = []
+            for cur_c in cur_ltrs:
+                for letter_comb in letter_combs:
+                    res.append(cur_c+letter_comb)
+
+            return res
+        
+s = Solution()
+r = s.letterCombinations('234')
+print(r)
